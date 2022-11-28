41bf48150f119c15de195e62833edea02903c5f6
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-28 11:15:17 +0200

    Add project files.

---
+++ L1 E9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SwappingValues
+{
+    /*  E9. Scrieti un program care interschimba 
+            valoarea a doua variabile intregi
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int x;
+            int y;
+
+            Console.WriteLine("Introduceti doua numere \n");
+            x = int.Parse(Console.ReadLine());
+            y = int.Parse(Console.ReadLine());
+
+            int z;
+            z = x;
+            x = y;
+            y = z;
+           
+            Console.WriteLine("Numerele dupa interschimbare sunt:");
+            Console.WriteLine($"{x}, {y}");
+        }
+    }
+}
